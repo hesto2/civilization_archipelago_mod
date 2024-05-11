609631da5f2456c0acf9ade24ca5a5cd64bffc8b
Carter Hesterman <hestermancarter@gmail.com>
2024-05-11 01:32:21 -0600

    fix bug with randomized future prereqs

--- ArchipelagoDBOverrides.sql
+++ ArchipelagoDBOverrides.sql
@@ -1,2 +1,10 @@
 -- Make it so policies that are required for specific governments can now be used with any government
-DELETE FROM Policy_GovernmentExclusives_XP2 WHERE 1 = 1
+DELETE FROM Policy_GovernmentExclusives_XP2 WHERE 1 = 1;
+UPDATE Technologies_XP2 set
+       RandomPrereqs = 0,
+       HiddenUntilPrereqComplete = 0
+ WHERE 1 = 1;
+UPDATE Civics_XP2 set
+       RandomPrereqs = 0,
+       HiddenUntilPrereqComplete = 0
+ WHERE 1 = 1;
